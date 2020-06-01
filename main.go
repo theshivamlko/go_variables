1d4b25ba1f71bc50b6e290d6ee4ffdf5cff47b18
Shivam <shivamsrivastava.9@gmail.com>
2020-06-01 08:17:13 +0530

    Create main.go
    initial

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 123
+
+	num2 := 222
+
+	num2 = 909
+
+	fmt.Println(num)
+	fmt.Println(num2)
+
+	var isAbsent bool
+	fmt.Println(isAbsent)
+
+	var str string = "My First Go Program"
+	fmt.Println(str)
+
+	str = "It executed successfully"
+	fmt.Println(str)
+
+	pi := 3.14
+	fmt.Println(pi)
+
+	pi = 2.14
+	fmt.Println(pi)
+}
